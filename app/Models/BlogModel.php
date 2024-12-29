788f33d08e9cf4c7eacd6cfa0f674c2dfe6d1afb
Sumayyah Ahmed <siddiquiaziz71@yahoo.com>
2024-12-29 11:19:05 -0600

    Update BlogModel.php

--- app/Models/BlogModel.php
+++ app/Models/BlogModel.php
@@ -1,34 +1,20 @@
 <?php namespace App\Models;
-
 use CodeIgniter\Model;
 
 class BlogModel extends Model
 {
     protected $table         = 'posts';
     protected $primaryKey    = 'post_id';
-
-   // protected $returnType  = 'array';
-   // protected $useSoftDeletes = true;
+    //protected $returnType  = 'array';
+    // protected $useSoftDeletes = true;
+    
     protected $allowedFields = ['post_title', 'post_content'];
-
-   protected $useTimestamps = true;
-   protected $createdField = 'post_created_at';
-   protected $updatedField = 'updated_at';
-
-
+    protected $useTimestamps = true;
+    protected $createdField = 'post_created_at';
+    protected $updatedField = 'updated_at';
+    
     //protected $deletedField = 'deleted_at';
-   // protected $validationRules = [];
-   // protected $validationMessages = [];
-   // protected $skipValidation = false;
-
+    // protected $validationRules = [];
+    // protected $validationMessages = [];
+    // protected $skipValidation = false;
 }
-
-
-
-
-
-
-
-
-
-
