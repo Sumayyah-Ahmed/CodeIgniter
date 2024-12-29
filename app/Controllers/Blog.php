d621ea342f401545a33d5426c1b51fefe2951771
Sumayyah Ahmed <siddiquiaziz71@yahoo.com>
2024-12-29 11:09:29 -0600

    Update Blog.php

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -1,61 +1,49 @@
 <?php namespace App\Controllers;
 
 use App\Models\BlogModel;
-
 class Blog extends BaseController
 {
     public function index()
     {
-
         $data = [
             //'meta_title' => 'Codeigniter 4 Blog',
-            'title' => 'This is a Blog Page',
-
-        ];
+             'title' => 'This is a Blog Page',
+            ];
 
         $posts = ['Title 1', 'Title 2', 'Title 3'];
         $data['posts'] = $posts;
-
         return view('blog', $data);
-
-      
     }
 
     public function post(){
         $model = new BlogModel();
         $post = $model->find();
         if($post){
-
-        $data = [
+            
+            $data = [
             'meta_title' => $post['post_title'],
             'title' => $post['post_title'],
             'post' => $post
-
-        ];
-    }else{
-        $data = [
+                ];
+        }else{
+            $data = [
             'meta_title' => 'Post Not Found',
             'title' => 'Post Not Found',
-        ];
+                ];
         }
-    
-
         return view('single_post', $data);
-
-
     }
 
     public function new(){
         $data = [
             'meta_title' => 'New Post',
             'title' => 'Create new post',
-
-        ];
+            ];
+        
         if($this->request->getMethod() == 'post'){
             $model = new BlogModel();
             $model->save($_POST);
         }
-
         return view('new_post', $data);
     }
 
@@ -70,21 +58,17 @@ class Blog extends BaseController
 
 
     public function edit($id){
-
         $model = new BlogModel();
         $post = $model->find($id);
-         $data = [
+        $data = [
             'meta_title' => $post['post_title'],
             'title' => $post['post_title'],
             'post' => $post
-
-        ];
-
+            ];
         if($this->request->getMethod() == 'post'){
             $model = new BlogModel();
             $model->save($_POST);
         }
-
         return view('edit_post', $data);
     }
 
