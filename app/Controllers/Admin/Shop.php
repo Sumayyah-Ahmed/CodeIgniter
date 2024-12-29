4e5026818ba1702153fd2d1d361bf18ed6eb3569
Sumayyah Ahmed <siddiquiaziz71@yahoo.com>
2024-12-29 11:14:15 -0600

    Update Shop.php

--- app/Controllers/Admin/Shop.php
+++ app/Controllers/Admin/Shop.php
@@ -1,10 +1,9 @@
 <?php namespace App\Controllers\Admin;
-
 use App\Controllers\BaseController;
+
 class Shop extends BaseController
 {
     public function index()
-
     {
         echo '<h2>This is an Admin Shop Area</h2>';
         //return view('shop');
