f47aa2a5ee1dface2ceaa5844a0f3e661d0c3236
Sumayyah Ahmed <siddiquiaziz71@yahoo.com>
2024-12-29 11:37:03 -0600

    Update blog_form.php

--- app/Views/blog_form.php
+++ app/Views/blog_form.php
@@ -1,13 +1,12 @@
 <form action="/admin/blog/new" method="post">
-<div>
-<lable for=""></lable>Title</lable>
-<input type="text" name ="title">
-</div>
-<div>
-<lable for="">Content</lable>
-<textarea name="content" id ="" cols="30" rows="10"></textarea>
-</div>
-<input type="submit" value="Create">
+  <div>
+    <lable for=""></lable>Title</lable>
+    <input type="text" name ="title">
+  </div>
+  <div>
+    <lable for="">Content</lable>
+    <textarea name="content" id ="" cols="30" rows="10"></textarea>
+  </div>
+  <input type="submit" value="Create">
 </form>
-
 
