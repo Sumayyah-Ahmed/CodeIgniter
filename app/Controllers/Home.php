342a1861d65c54be5a8df2feafbcd1529662fda1
Sumayyah Ahmed <siddiquiaziz71@yahoo.com>
2024-12-29 11:10:45 -0600

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,20 +1,18 @@
 <?php namespace App\Controllers;
-
 use App\Controllers\Admin\Shop as AdminShop;
+
 class Home extends BaseController
 {
     public function index(): string
     {
         return view('welcome_message');
     }
-
     function validation(){ 
         $shop = new Shop();
         echo $shop->product('laptop', 'lenovo'). '<br>';
 
         $adminShop = new AdminShop();
         echo $adminShop->product('laptop', 'lenovo');
-        
     }
     function show404(){
         echo 'This is a 404 page';
